9005700a83cdae704912814f8b7508128449ced4
p0dalirius <podalirius@protonmail.com>
2022-01-21 10:38:52 +0100

    Added try statements to skip broken isos

--- WindowsBuildFromISO.py
+++ WindowsBuildFromISO.py
@@ -13,6 +13,7 @@ import xml.etree.ElementTree as ET
 
 
 def check_windows_version(isofile, verbose=False):
+    versions, version_string, display_name, languages_string = None,None,None,None
     # Mount iso read-only
     oldpwd = os.getcwd()
     iso_mount_dir = tempfile.TemporaryDirectory()
@@ -29,58 +30,61 @@ def check_windows_version(isofile, verbose=False):
 
     # Parse files in the ISO to get the Windows build
     os.chdir(work_dir.name)
-    if os.path.isfile(iso_mount_dir.name + "/sources/install.wim"):
-        if verbose:
-            print("   [>] Parsing Windows build number ...")
-        if verbose:
-            print("   [>] Extracting install.wim '[1].xml' file ...")
-            os.system("7z e '%s/sources/install.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name)
-        else:
-            os.popen("7z e '%s/sources/install.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name).read()
-        if verbose:
-            print("   [>] Parsing install.wim '[1].xml' file ...")
-        tree = ET.parse('[1].xml')
-        image = tree.findall('IMAGE')[0]
-        windows = image.find('WINDOWS')
-        # Versions
-        versions = {c.tag.lower(): c.text for c in list(windows.find('VERSION'))}
-        version_string = "%s.%s.%s.%s" % (versions['major'], versions['minor'], versions['build'], versions['spbuild'])
-        # Languages
-        languages = [l.text for l in windows.findall('LANGUAGES/LANGUAGE')]
-        languages_string = ("multi" if len(languages) > 1 else languages[0].lower())
-        # Display name
-        display_name = image.find('DISPLAYNAME').text.strip()
-        if "Evaluation" in display_name:
-            display_name = display_name.replace("Evaluation", "").strip()
-
-    elif os.path.isfile(iso_mount_dir.name + "/sources/boot.wim"):
-        if verbose:
-            print("   [>] Parsing Windows build number ...")
-        if verbose:
-            print("   [>] Extracting boot.wim '[1].xml' file ...")
-            os.system("7z e '%s/sources/boot.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name)
-        else:
-            os.popen("7z e '%s/sources/boot.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name).read()
-        if verbose:
-            print("   [>] Parsing boot.wim '[1].xml' file ...")
-        tree = ET.parse('[1].xml')
-        image = tree.findall('IMAGE')[0]
-        windows = image.find('WINDOWS')
-        # Versions
-        versions = {c.tag.lower(): c.text for c in list(windows.find('VERSION'))}
-        version_string = "%s.%s.%s.%s" % (versions['major'], versions['minor'], versions['build'], versions['spbuild'])
-        # Languages
-        languages = [l.text for l in windows.findall('LANGUAGES/LANGUAGE')]
-        languages_string = ("multi" if len(languages) > 1 else languages[0].lower())
-        # Display name
-        display_name = image.find('DISPLAYNAME')
-        if display_name is not None:
-            display_name = display_name.text.strip()
-        else:
-            if versions['minor'] != "0":
-                display_name = "Windows %s.%s" % (versions['major'], versions['minor'])
+    try:
+        if os.path.isfile(iso_mount_dir.name + "/sources/install.wim"):
+            if verbose:
+                print("   [>] Parsing Windows build number ...")
+            if verbose:
+                print("   [>] Extracting install.wim '[1].xml' file ...")
+                os.system("7z e '%s/sources/install.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name)
             else:
-                display_name = "Windows %s" % (versions['major'])
+                os.popen("7z e '%s/sources/install.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name).read()
+            if verbose:
+                print("   [>] Parsing install.wim '[1].xml' file ...")
+            tree = ET.parse('[1].xml')
+            image = tree.findall('IMAGE')[0]
+            windows = image.find('WINDOWS')
+            # Versions
+            versions = {c.tag.lower(): c.text for c in list(windows.find('VERSION'))}
+            version_string = "%s.%s.%s.%s" % (versions['major'], versions['minor'], versions['build'], versions['spbuild'])
+            # Languages
+            languages = [l.text for l in windows.findall('LANGUAGES/LANGUAGE')]
+            languages_string = ("multi" if len(languages) > 1 else languages[0].lower())
+            # Display name
+            display_name = image.find('DISPLAYNAME').text.strip()
+            if "Evaluation" in display_name:
+                display_name = display_name.replace("Evaluation", "").strip()
+
+        elif os.path.isfile(iso_mount_dir.name + "/sources/boot.wim"):
+            if verbose:
+                print("   [>] Parsing Windows build number ...")
+            if verbose:
+                print("   [>] Extracting boot.wim '[1].xml' file ...")
+                os.system("7z e '%s/sources/boot.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name)
+            else:
+                os.popen("7z e '%s/sources/boot.wim' '[1].xml' -aoa 2>&1" % iso_mount_dir.name).read()
+            if verbose:
+                print("   [>] Parsing boot.wim '[1].xml' file ...")
+            tree = ET.parse('[1].xml')
+            image = tree.findall('IMAGE')[0]
+            windows = image.find('WINDOWS')
+            # Versions
+            versions = {c.tag.lower(): c.text for c in list(windows.find('VERSION'))}
+            version_string = "%s.%s.%s.%s" % (versions['major'], versions['minor'], versions['build'], versions['spbuild'])
+            # Languages
+            languages = [l.text for l in windows.findall('LANGUAGES/LANGUAGE')]
+            languages_string = ("multi" if len(languages) > 1 else languages[0].lower())
+            # Display name
+            display_name = image.find('DISPLAYNAME')
+            if display_name is not None:
+                display_name = display_name.text.strip()
+            else:
+                if versions['minor'] != "0":
+                    display_name = "Windows %s.%s" % (versions['major'], versions['minor'])
+                else:
+                    display_name = "Windows %s" % (versions['major'])
+    except Exception as e:
+        print("   [\x1b[91merror\x1b0m] %s" % e)
 
     os.chdir(oldpwd)
     # Unmount ISO and remove temporary directory
@@ -107,6 +111,7 @@ def archive_iso(archive_dir, iso, versions, version_string, display_name, langua
         os_type = display_name
 
     basedir = "%s/%s/%s - %s/%s/" % (archive_dir, os_type, version_string, display_name, languages_string)
+    # basedir = basedir.replace(' ', '_')
     isoname = display_name.replace(' ', '_')
     if "branch" in versions.keys():
         isoname = "%s.%s.%s.%s.iso" % (version_string, isoname, versions['branch'], languages_string)
@@ -164,10 +169,13 @@ if __name__ == '__main__':
             print("[!] Cannot access ISO file. Wrong path or bad permissions maybe ?")
             sys.exit(-1)
         print("[iso] %s " % options.iso)
-        versions, version_string, display_name, languages_string = check_windows_version(options.iso, verbose=options.verbose)
-        print_windows_version(versions, display_name, languages_string, no_colors=options.no_colors)
-        if options.archive_dir is not None:
-            archive_iso(options.archive_dir, options.iso, versions, version_string, display_name, languages_string, verbose=options.verbose)
+        try:
+            versions, version_string, display_name, languages_string = check_windows_version(options.iso, verbose=options.verbose)
+            print_windows_version(versions, display_name, languages_string, no_colors=options.no_colors)
+            if options.archive_dir is not None:
+                archive_iso(options.archive_dir, options.iso, versions, version_string, display_name, languages_string, verbose=options.verbose)
+        except Exception as e:
+            print("   [\x1b[91merror\x1b0m] %s" % e)
     elif options.iso_dir is not None:
         if not os.path.isdir(options.iso_dir):
             print("[!] Cannot access ISO directory. Wrong path or bad permissions maybe ?")
@@ -182,8 +190,11 @@ if __name__ == '__main__':
                     iso_files.append(name)
         for iso in iso_files:
             print("[iso] %s " % iso)
-            versions, version_string, display_name, languages_string = check_windows_version(iso, verbose=options.verbose)
-            print_windows_version(versions, display_name, languages_string, no_colors=options.no_colors)
-            if options.archive_dir is not None:
-                archive_iso(options.archive_dir, iso, versions, version_string, display_name, languages_string, verbose=options.verbose)
+            try:
+                versions, version_string, display_name, languages_string = check_windows_version(iso, verbose=options.verbose)
+                print_windows_version(versions, display_name, languages_string, no_colors=options.no_colors)
+                if options.archive_dir is not None:
+                    archive_iso(options.archive_dir, iso, versions, version_string, display_name, languages_string, verbose=options.verbose)
+            except Exception as e:
+                print("   [\x1b[91merror\x1b0m] %s" % e)
 
